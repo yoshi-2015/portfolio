fc0fd6777e1af40de6f4fa983d33d9a9ca054f5b
yoshi-2015 <yoshihisa.ohkura@icloud.com>
2020-01-24 07:42:22 +0000

    [Add]movie_comment create/destroy + Asynchronous communication

--- app/controllers/movie_comments_controller.rb
+++ app/controllers/movie_comments_controller.rb
@@ -1,15 +1,21 @@
 class MovieCommentsController < ApplicationController
   def create
-    movie = Movie.details(params[:id])
+    # binding.pry
     @movie_comment = current_user.movie_comments.new(movie_comment_params)
+    movie_id = @movie_comment.movie_id
+    @movie_comments = MovieComment.where(movie_id: movie_id)
     if @movie_comment.save
-      redirect_to movie_path(movie)
     else
       render template: "memos/show"
     end
   end
 
   def destroy
+    # binding.pry
+    @movie_comment = MovieComment.find(params[:id])
+    @movie_comment.destroy
+    movie_id = @movie_comment.movie_id
+    @movie_comments = MovieComment.where(movie_id: movie_id)
   end
 
   def movie_comment_params
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,9 +9,9 @@ class MoviesController < ApplicationController
   def show
     @movie_info = Movie.details(params[:id])
     @cast_info = Movie.castdetails(params[:id])["cast"]
-    # binding.pry
     @movie_comment = MovieComment.new
     apimovie_id = @movie_info["id"]
     @movie_comments = MovieComment.where(movie_id: apimovie_id)
+    # binding.pry
   end
 end
